5cecc294cee1873df67150028d54ba559ed60413
AB7zz <48093418+AB7zz@users.noreply.github.com>
2021-05-17 18:25:19 +0530

    Create db.php

---
+++ db/db.php
@@ -0,0 +1,6 @@
+<?php
+// $con = mysqli_connect("localhost", "tujjxspt_Abbas", 'Lacoste123', "tujjxspt_tujjari", '3306');
+// if($con=== false){ die("ERROR: Could not connect. " . mysqli_connect_error()); }
+$con = mysqli_connect("localhost", "root", "", "tujjari");
+if($con=== false){ die("ERROR: Could not connect. " . mysqli_connect_error()); }
+?>
